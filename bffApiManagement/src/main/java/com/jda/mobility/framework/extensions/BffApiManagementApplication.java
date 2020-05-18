b6db91c4a9d9351e2c8f258e825c2eb31b297b84
lakshmanan03 <59009886+lakshmanan03@users.noreply.github.com>
2020-05-18 12:22:52 +0530

    Add files via upload

---
+++ bffApiManagement/src/main/java/com/jda/mobility/framework/extensions/BffApiManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jda.mobility.framework.extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffApiManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffApiManagementApplication.class, args);
+	}
+
+}
